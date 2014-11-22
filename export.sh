128449fb2d8158667b574fc5af00d2c289565601
Caleb Evans <calebevans.me@gmail.com>
2014-11-22 08:32:33 -0800

    Added support for non-synced installs in export script

--- export.sh
+++ export.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
 
-# Global Alfred properties
-
 # Path to Alfred preferences PLIST file
-alfred_prefs_plist=~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist
-# Path to directory in which Alfred preferences are synced
-alfred_sync_dir=$(defaults read "$alfred_prefs_plist" syncfolder)
-# Path to Alfred preferences
-workflows=$(eval echo $alfred_sync_dir/Alfred.alfredpreferences/workflows/user.workflow.*/)
-workflow_dir=""
+alfred_plist=~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist
 
-# Workflow properties
+# Path to directory in which Alfred preferences are synced
+alfred_prefs_dir=$(defaults read "$alfred_plist" syncfolder 2> /dev/null)
+
+# If no sync folder is set
+if [[ $alfred_prefs_dir == '' ]]; then
+	# Use default location for preferences
+	alfred_prefs_dir="~/Library/Application Support/Alfred 2"
+fi
+
+# Installed Alfred workflows
+workflows=$(eval echo "$alfred_prefs_dir"/Alfred.alfredpreferences/workflows/user.workflow.*/)
+
+# Directory for installed workflow
+workflow_dir=""
 
 # Name of the workflow file (w/o extension)
 workflow_name="Play Song"
+
 # Permanent bundle ID for the workflow
 bundle_id="com.calebevans.playsong"
 
-# Workflow-related paths
-
 # Path to project repository
 project_dir=$(dirname $0)
+
 # Path to plain-text workflow configuration
 project_config="$project_dir/Configuration.applescript"
+
 # Path to script used for compiling configuration
 project_config_compile="$project_dir/compile.sh"
+
 # Path to zip file used to create workflow file
 workflow_zip_file="$project_dir/$workflow_name.zip"
+
 # Path to resulting workflow file
 workflow_file="$project_dir/$workflow_name.alfredworkflow"
 
-echo "Locating workflow directory..."
 # Locate workflow directory from bundle ID
+echo "Locating workflow directory..."
 for workflow in $workflows
 do
 	# If workflow PLIST lists the bundle ID
