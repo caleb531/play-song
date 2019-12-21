767a43d74cf5d416708afb7abe41c65a3bae3a20
Caleb Evans <caleb@calebevans.me>
2019-12-20 16:28:39 -0800

    Add tests for clearcache action

--- tests/utils.py
+++ tests/utils.py
@@ -16,3 +16,11 @@ def run_filter(filter_name, filter_arg):
     ])
 
     return json.loads(raw_feedback)['items']
+
+
+def run_action(action_name):
+
+    subprocess.check_output([
+        'osascript',
+        'actions/{}.applescript'.format(action_name)
+    ])
