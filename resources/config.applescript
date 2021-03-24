caleb531/play-song

commit a2ec45c0fa19bdde02fa206ac453c13f95e688aa

Make playartist filter faster and honor sort order

resources/config.applescript
@@ -264,44 +264,12 @@ on getGenreSongs(genreName)
 
 end getGenreSongs
 
--- retrieves list of album names for the given artist
-on getArtistAlbums(artistName)
-
-	tell application "Music"
-
-		set artistSongs to every track of playlist 2 whose artist is artistName
-		set albumNames to {}
-
-		repeat with theSong in artistSongs
-
-			if (album of theSong) is not in albumNames then
-
-				set albumNames to albumNames & (album of theSong)
-
-			end if
-
-		end repeat
-
-	end tell
-
-	return albumNames
-
-end getArtistAlbums
-
 -- retrieves list of songs by the given artist, sorted by album
 on getArtistSongs(artistName)
 
 	tell application "Music"
 
-		set albumNames to getArtistAlbums(artistName) of me
-		set artistSongs to {}
-
-		repeat with albumName in albumNames
-
-			set albumSongs to (every track of playlist 2 whose artist is artistName and album is albumName)
-			set artistSongs to artistSongs & albumSongs
-
-		end repeat
+		set artistSongs to every track of playlist 2 whose artist is artistName
 
 	end tell
 

commit 74fe089b497feaab0568de327f7f2e6aad57c3fc

Fix regression where a2ec45c broke playalbumby filter
This is because a2ec45c removed the getArtistAlbums handler, which is
used by the playalbumby filter.

resources/config.applescript
@@ -266,6 +266,30 @@ on getGenreSongs(genreName)
 
 end getGenreSongs
 
+-- retrieves list of album names for the given artist
+on getArtistAlbums(artistName)
+
+    tell application "Music"
+
+        set artistSongs to every track of playlist 2 whose artist is artistName
+        set albumNames to {}
+
+        repeat with theSong in artistSongs
+
+            if (album of theSong) is not in albumNames then
+
+                set albumNames to albumNames & (album of theSong)
+
+            end if
+
+        end repeat
+
+    end tell
+
+    return albumNames
+
+end getArtistAlbums
+
 -- retrieves list of songs by the given artist, sorted by album
 on getArtistSongs(artistName)
 